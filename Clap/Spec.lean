9595c39f1c9add83bcd2f1223f90edfb064512b6
Marco Stronati <marco@stronati.org>
2026-01-09 17:05:36 +0100

    Spec: clean

--- Clap/Spec.lean
+++ Clap/Spec.lean
@@ -5,10 +5,9 @@ import Clap.Simulation
 
 namespace Clap
 
-namespace Spec
+variable {p : ℕ} [Fact (Nat.Prime p)]
 
---variable (F var:Type) [Field F] [DecidableEq F]
-variable {p : ℕ} [Fact (Nat.Prime p)] [NeZero p]
+namespace Spec
 
 def eq0 (e : ZMod p) : Option Unit :=
   if e = 0 then some () else none
@@ -19,55 +18,12 @@ def is_zero (e : ZMod p) : ZMod p := if e = 0 then 1 else 0
 
 def assert_range (w : ℕ) (e : ZMod p) : Option Unit := if e.val < 2^w then some () else none
 
---def assert_range (w:ℕ) (e:ZMod p) : Option { x:ZMod p // (x.val < 2^w) } := if h:e.val < 2^w then some ⟨e, h⟩ else none
-
--- def assert_range (w:ℕ) (e:ZMod p) : Option (ZMod p') := if e.val < 2^w then some e else none
-
 def div_rem (e:ZMod p) : Option (ZMod p × ZMod p) :=
   let d := e / (2^8)
   let r := e % (2^8)
   (d,r)
 
 def accept : Unit -> Unit := fun () => ()
-
---lemma lemma_range : ∀ w (e:ZMod p), assert_range w e = some () -> e.val < 2^w
-
-------------------
-
--- def to_u4 (e:ZMod p) : Option (Fin 4) :=
---   let := assert_range 2 e -- outside the do we keep the assert_range
---   do
---   let x <- assert_range 2 e
---   let res : Fin 4 := ⟨e.val, b
-
--- def to_u4 (e:ZMod p) : Option (Fin 4) := do
---   let x <- assert_range 2 e
---   let res : Fin 4 := ⟨x.1, by  sorry ⟩
---   some res
-
-def to_u4 (e : ZMod p) : Option (ZMod p) := do
-  assert_range 2 e
-  some e
-
-instance : LT (ZMod p) where
-  lt := fun x y ↦ x.val < y.val
-
-@[simp]
-def zmod_lt_def {a b : ZMod p} : a < b ↔ a.val < b.val := by
-  rfl
-
--- lemma to_u4_ok (h : 4 < p) : ∀ e, to_u4 e < some (4 : ZMod p) := by
---   aesop (add simp [to_u4, assert_range, Fin.lt_def, Nat.mod_eq_of_lt, ZMod, ZMod.val]) (add safe cases Fin)
-
-omit [NeZero p] in
-lemma to_u4_ok (h : 4 < p) : ∀ e, to_u4 e < some (4 : ZMod p) := by
-  have : ZMod.val (4 : ZMod p) = 4 := by
-      exact ZMod.val_natCast_of_lt h
-  aesop (add simp [to_u4, assert_range])
-
-
-
-  -- aesop (add simp [to_u4, assert_range, Fin.lt_def, Nat.mod_eq_of_lt]) (add safe cases ZMod)
 
 /-
   Expand a function that takes a vector of n Felts, into a series of n
@@ -87,7 +43,6 @@ def curry {a r : Type} (n : ℕ) (k : Vector a n -> r) : typ a r n :=
 
 #guard curry 2 (fun x => x[0] == 0 && x[1] == 1) 1 0 = True
 
-omit [NeZero p] in
 lemma equiv_eq0 (el : ZMod p) (er : Exp (ZMod p) (ZMod p)) (cl : Option Unit) (cr : Circuit p (ZMod p)) :
     el = Exp.eval er ->
     Simulation.s_bisim cl (Circuit.eval cr) ->
@@ -110,7 +65,6 @@ lemma equiv_eq0 (el : ZMod p) (er : Exp (ZMod p) (ZMod p)) (cl : Option Unit) (c
     . apply hc
     . contradiction
 
-omit [NeZero p] in
 lemma equiv_share (el : ZMod p) (er : Exp (ZMod p) (ZMod p)) (kl : ZMod p -> Option Unit) (kr : ZMod p -> Circuit p (ZMod p)) :
   el = Exp.eval er ->
   (∀ x, Simulation.s_bisim (kl x) (Circuit.eval (kr x))) ->
@@ -150,8 +104,6 @@ namespace Example_base
 
 open Spec
 
-variable {p:ℕ} [Fact (Nat.Prime p)] [NeZero p]
-
 /-
   A circuit is a function from any number of arguments of type F or Vector F to Option Unit.
 -/
@@ -170,7 +122,7 @@ def ex p (i: ZMod p) : Option Unit := do
 --   bind (eq0 F (vi + i)) (fun () =>
 --   some ())))
 
-def ex_circuit_fun (p : ℕ) [Fact (Nat.Prime p)] : Circuit' p := fun _ =>
+def ex_circuit_fun (p : ℕ) : Circuit' p := fun _ =>
   .lam (fun i =>
   .eq0 (.v i) (
   .share (.v i) (fun vi =>
@@ -197,9 +149,8 @@ theorem equiv :
     constructor
     constructor
 
-theorem extract : ∀ p [Fact (Nat.Prime p)],
+theorem extract :
   ∃ c : Circuit p (ZMod p), Simulation.s_bisim (ex p) (Circuit.eval c) := by
-  intro p _
   unfold ex
   simp only [bind]
   refine ⟨?c,?p⟩
@@ -220,14 +171,15 @@ def ex p (is : Vector (ZMod p) 2) : Option Unit := do
   eq0 (vi + is[1])
   accept ()
 
-def ex_circuit_fun (p : ℕ) [Fact (Nat.Prime p)] : Circuit' p := fun _ =>
+def ex_circuit_fun (p : ℕ) : Circuit' p := fun _ =>
   Circuit.curry 2 (fun is =>
   .eq0 (.v is[0]) (
   .share (.v is[0]) (fun vi =>
   .eq0 (.v vi + .v is[1]) (
   .nil))))
 
-theorem equiv {p : ℕ} [Fact (Nat.Prime p)] : Simulation.s_bisim (curry 2 (ex p)) (Circuit.eval' (ex_circuit_fun p)) := by
+theorem equiv :
+  Simulation.s_bisim (curry 2 (ex p)) (Circuit.eval' (ex_circuit_fun p)) := by
   unfold ex_circuit_fun
   unfold ex
   simp only [bind]
@@ -248,8 +200,6 @@ end Example_vec
 
 namespace Example_fold
 
-variable {p : ℕ} [Fact (Nat.Prime p)]
-
 open Spec
 
 /- TODO these curry should disappear, the signature should be:
@@ -265,7 +215,7 @@ def ex p :=
   return accept ()
   )))
 
-def ex_circuit_fun (p : ℕ) [Fact (Nat.Prime p)] : Circuit' p := fun _ =>
+def ex_circuit_fun (p : ℕ) : Circuit' p := fun _ =>
   Circuit.curry 2 (fun xs =>
   Circuit.curry 2 (fun ys =>
   Circuit.curry 2 (fun zs =>
