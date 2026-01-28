585bc2853031038ea85c1fa0de3abdba1a498520
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2026-01-28 10:12:20 +0100

    Cfold: cleanup

--- Clap/Cfold.lean
+++ Clap/Cfold.lean
@@ -8,84 +8,63 @@ namespace Clap
   This optimization does not depend on any choice of var.
 -/
 
-namespace Cfold
-
-variable {var : Type}
-variable {p : ℕ} [Fact (Nat.Prime p)]
+variable {var : Type} {p : ℕ}
 
 /-
   Note: this function contains some pattern matchings that don't look ideal
   because of https://github.com/leanprover/lean4/issues/9803
   Typically "| l,.c 0 => " is replaced by "| l,r => if r=0 then"
 -/
-def cfold_e (e : Exp p var) : Exp p var :=
+def Exp.cfold (e : Exp p var) : Exp p var :=
   match e with
-  | .v v => .v v
+  | .v var => .v var
   | .c i => .c i
   | .add l r =>
-    match cfold_e l, cfold_e r with
+    match l.cfold, r.cfold with
     | .c l , .c r => .c (l+r)
-    |    l , .c r => if r=0 then l else .add l r
-    | .c l , r    => if l=0 then r else .add l r
+    |    l , .c r => if r = 0 then l else .add l r
+    | .c l , r    => if l = 0 then r else .add l r
     |    _ , _    => .add l r
   | .mul l r =>
-    match cfold_e l, cfold_e r with
+    match l.cfold, r.cfold with
     | .c l , .c r => .c (l * r)
     | .c l , r    =>
-      if l=0 then .c 0 else
-      if l=1 then r
+      if l = 0 then .c 0 else
+      if l = 1 then r
       else .mul l r
     |    l , .c r =>
-      if r=0 then .c 0 else
-      if r=1 then l
+      if r = 0 then .c 0 else
+      if r = 1 then l
       else .mul l r
     |    _ , _    => .mul l r
   | .sub l r =>
-    match cfold_e l, cfold_e r with
+    match l.cfold, r.cfold with
     | .c l , .c r => .c (l-r)
-    |    l , .c r => if r=0 then l else .sub l r
+    |    l , .c r => if r = 0 then l else .sub l r
     |    _ , _    => .sub l r
 
-def cfold {var : Type} (c : Circuit p var) : Circuit p var :=
+def Circuit.cfold {var : Type} (c : Circuit p var) : Circuit p var :=
   match c with
   | .nil => .nil
-  | .eq0 e c => .eq0 (cfold_e e) (cfold c)
-  | .lam k => .lam (fun x => cfold (k x))
-  | .share e k => .share (cfold_e e) (fun x => cfold (k x))
-  | .is_zero e k => .is_zero (cfold_e e) (fun x => cfold (k x))
+  | .eq0 e c => .eq0 e.cfold c.cfold
+  | .lam k => .lam fun x => (k x).cfold
+  | .share e k => .share e.cfold fun x => (k x).cfold
+  | .is_zero e k => .is_zero e.cfold fun x => (k x).cfold
 
-def cfold' (c : Circuit' p) : Circuit' p := fun var => cfold (c var)
+def cfold' (c : Circuit' p) : Circuit' p := fun var => Circuit.cfold (c var)
 
--- TODO change to ≈
-theorem cfold_e_sem_pre : ∀ (e : Expₑ p), Exp.eval e = Exp.eval (cfold_e e) := by
-  intros e
-  induction e with
-  | v f
-  | c f =>
-    simp [cfold_e,Exp.eval]
-  | add l r hl hr
-  | mul l r hl hr
-  | sub l r hl hr =>
-    simp [cfold_e,Exp.eval]
-    repeat (split <;> repeat simp [*,Exp.eval])
+@[simp]
+theorem Exp.cfold_sem_pre {e : Expₑ p} : e.cfold ≈ e := by
+  induction e <;> aesop (add simp [Exp.equiv_iff_eval_eq_eval, Exp.eval, Exp.cfold])
 
-theorem cfold_sem_pre : ∀ (c : Circuitₑ p),
-  c ≈ cfold c := by
-  intros c
-  induction c with
-  | nil =>
-    constructor
-  | lam k h
-  | eq0 e c h
-  | share e c h
-  | is_zero e c h =>
-    simp [cfold]
-    gcongr
-    repeat (first | apply cfold_e_sem_pre | apply h)
+theorem cfold_sem_pre {c : Circuitₑ p} : c ≈ c.cfold := by
+  induction c <;> unfold Circuit.cfold
+  all_goals gcongr; try grw [Exp.cfold_sem_pre]
+  all_goals grind
 
 theorem cfold'_sem_pre : ∀ (c : Circuit' p),
   Circuit.eval' c = Circuit.eval' (cfold' c) := by
   intros
   apply cfold_sem_pre
 
-end Cfold
+end Clap
