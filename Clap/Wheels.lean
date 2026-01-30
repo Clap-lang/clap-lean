8bb59463d1cf9bf559c020fffe80471b2a2eee97
Marco Stronati <marco@stronati.org>
2026-01-30 15:52:04 +0100

    Circuit: fix some num2bits leftovers

--- Clap/Wheels.lean
+++ Clap/Wheels.lean
@@ -42,8 +42,18 @@ elab "reduce_curry" : tactic => Elab.Tactic.liftMetaTactic' reduceCurry
 
 end
 
+/-- Computes minimum number of bits necessary to represent the input. -/
+def minBits (x : ℕ) : ℕ :=
+  if x = 0 then 1 else
+  let nb := Nat.log2 x
+  if 2^nb ≤ x then nb + 1 else nb
+
 variable {p : ℕ} [Fact (Nat.Prime p)]
 
+/-- Computes the `n` bit binary representation of `f`.
+    If `n < minBits f` the result is truncated.
+    If `n > minBits f` the result is padded with zeros.
+-/
 def num2bits_pure (n:ℕ) (f:ZMod p) : List (ZMod p) :=
   match n with
   | 0 => []
