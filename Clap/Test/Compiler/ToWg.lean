62746e2f82d5f95acf39406c841e15853993365e
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2026-02-19 16:31:41 +0100

    redo the toWg test

--- Clap/Test/Compiler/ToWg.lean
+++ Clap/Test/Compiler/ToWg.lean
@@ -10,31 +10,26 @@ namespace Compiler
 
 open Lean Clap Meta Spec Compiler
 
-variable {p : Nat} [Fact (Nat.Prime p)]
-
 structure Point (p : ℕ) where
   x : ZMod p
   y : ZMod p
   z : ZMod p
 
-def ToWg.ex₁_aux {p : Nat} [Fact (Nat.Prime p)] (var : Type) : Circuit p var := .nil
-def ToWg.ex₁ {p : Nat} [Fact (Nat.Prime p)] (_point : Point p) : Option Unit := accept
+def ToWg.ex₁_aux (var : Type) : Circuit Primes.babybear var := .nil
+def ToWg.ex₁ (_point : Point Primes.babybear) : Option Unit := accept
 
 /--
-info: def -
-  fun {p : ℕ} [Fact (Nat.Prime p)] (_point : Point p) =>
-    (toWg' ToWg.ex₁_aux).run { toList := [_point.x, _point.y, _point.z] }
+info: def - fun (wg : Wg Primes.babybear) (_point : Point Primes.babybear) =>
+  wg.run { toList := [_point.x, _point.y, _point.z] }
 ---
-info: type -
-  {p : ℕ} → [Fact (Nat.Prime p)] → Point p → Array (ZMod p)
+info: type - Wg Primes.babybear → Point Primes.babybear → Array (ZMod Primes.babybear)
 -/
 #guard_msgs(info, whitespace := lax) in
 set_option pp.funBinderTypes true in
 run_elab do
   let decl ← find! `ToWg.ex₁
-  let declC ← find! `ToWg.ex₁_aux
   lambdaTelescope decl.value! fun args _ ↦ do
-    let toWgd ← wg declC.name args
+    let toWgd ← wg `Primes.babybear args
     logInfo m!"def - {toWgd}"
     logInfo m!"type - {←inferType toWgd}"
 
