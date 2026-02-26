31fd1a1d8e71639a1b92385f8d7fc9277997bebc
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2026-02-26 08:50:48 +0100

    Compiler: add linearise reduction, switch to Core, fix serialisation

--- Clap/Test/Compiler/ToDeep.lean
+++ Clap/Test/Compiler/ToDeep.lean
@@ -11,17 +11,16 @@ namespace Compiler
 open Lean Clap Meta
 
 open Spec Compiler in
-def ToDeep.ex₁ {p:ℕ} [Fact (Nat.Prime p)] (x:ZMod p) : Option Unit :=
-  let y : ZMod p := 1
-  let z : ZMod p := is_zero y
+def ToDeep.ex₁ (x : ZMod Primes.babybear) : Option Unit :=
+  let y : ZMod Primes.babybear := 1
+  let z : ZMod Primes.babybear := is_zero y
   do
   let _j <- num2bits 2 y
-  eq0 (x+(1:ZMod p) * x-y+z) -- cannot use j[0]!
+  eq0 (x+(1:ZMod Primes.babybear) * x-y+z) -- cannot use j[0]!
   accept
 
 /--
-info: def -
-fun (p : ℕ) [Fact (Nat.Prime p)] (var : Type) =>
+info: def - fun (var : Type) =>
   Circuit.lam fun (x : var) =>
     Circuit.share (Exp.c 1) fun (y : var) =>
       Circuit.is_zero (Exp.v y) fun (z : var) =>
@@ -29,14 +28,13 @@ fun (p : ℕ) [Fact (Nat.Prime p)] (var : Type) =>
           Circuit.eq0 ((((Exp.v x).add ((Exp.c 1).mul (Exp.v x))).sub (Exp.v y)).add (Exp.v z))
             ((fun (x : PUnit.{1}) => Circuit.nil) ())
 ---
-info: type -
-(p : ℕ) → [Fact (Nat.Prime p)] → (var : Type) → Circuit p var
+info: type - (var : Type) → Circuit Primes.babybear var
 -/
 #guard_msgs(info, whitespace := lax) in
 set_option pp.funBinderTypes true in
 run_elab do
   try
-    let deep ← toDeep (←find! `ToDeep.ex₁).value!
+    let deep ← toDeep `Primes.babybear (←find! `ToDeep.ex₁).value!
     logInfo m!"def - {deep}"
     logInfo m!"type - {←inferType deep}"
   catch e =>
@@ -46,19 +44,17 @@ run_elab do
 /- TODO we need to support the following two cases that are currently failing. -/
 
 open Spec Compiler in
-def ToDeep.ex₂ {p:ℕ} [Fact (Nat.Prime p)] (x:ZMod p) : Option Unit := do
+def ToDeep.ex₂ (x : ZMod Primes.babybear) : Option Unit := do
   let j <- num2bits 2 x
   eq0 (j[0]!)
   accept
 
-/--
-info: compileExp: no match
--/
+/-- info: compileExp: no match for vars[0]! -/
 #guard_msgs(info, whitespace := lax) in
 set_option pp.funBinderTypes true in
 run_elab do
   try
-    let deep ← toDeep (←find! `ToDeep.ex₂).value!
+    let deep ← toDeep `Primes.babybear (←find! `ToDeep.ex₂).value!
     logInfo m!"def - {deep}"
     logInfo m!"type - {←inferType deep}"
   catch e =>
@@ -66,18 +62,16 @@ run_elab do
 
 
 open Spec Compiler in
-def ToDeep.ex₃ {p:ℕ} [Fact (Nat.Prime p)] (x:ZMod p) : Option Unit := do
+def ToDeep.ex₃ (x : ZMod Primes.babybear) : Option Unit := do
   eq0 (is_zero x + share x)
   accept
 
-/--
-info: compileExp: no match
--/
+/-- info: compileExp: no match for Spec.Compiler.is_zero x -/
 #guard_msgs(info, whitespace := lax) in
 set_option pp.funBinderTypes true in
 run_elab do
   try
-    let deep ← toDeep (←find! `ToDeep.ex₃).value!
+    let deep ← toDeep `Primes.babybear (←find! `ToDeep.ex₃).value!
     logInfo m!"def - {deep}"
     logInfo m!"type - {←inferType deep}"
   catch e =>
