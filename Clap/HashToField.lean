84f4cbfe706cc3f5185cfd36c28ba7938f67e1ee
Andrei Burdusa <andrei.burdusa@zoho.com>
2026-03-04 16:33:58 +0200

    Finish hash64BitLimbsToFieldWithLen

--- Clap/HashToField.lean
+++ Clap/HashToField.lean
@@ -7,35 +7,25 @@ namespace HashToField
 open Clap.Lang
 
 abbrev p := Clap.Poseidon.p
+
 open Core
 
 variable [Core p]
 
--- Stolen from Poseidon.lean
-private def liftArr (xs : Array (ZMod p)) : Array (F p) := xs.map const
-private def liftMat (xs : Array (Array (ZMod p))) : Array (Array (F p)) := xs.map (·.map const)
-
+/-
+  `inputMaxbits` corresponds to the `{maxbits}` tag. Circom tags are assigned
+  values known at compile time.
+-/
 def hash64BitLimbsToFieldWithLen {numLimbs : ℕ}
   (input : Vector (F p) numLimbs)
-  (len : ℕ)
-  (_ : len > 0):
+  (inputMaxbits : ℕ)
+  (len : F p) :
   Option (F p)
 := do
+  if inputMaxbits > 64 || numLimbs == 0 then .none
   let elems ← Packing.chunksToFieldElems (p := p) 3 64 input
   let elems := elems.push len
-  let t := elems.size + 1
-  let C ← Clap.Poseidon.Constant.C[t-2]?
-  let S ← Clap.Poseidon.Constant.S[t-2]?
-  let M ← Clap.Poseidon.Constant.M[t-2]?
-  let P ← Clap.Poseidon.Constant.P[t-2]?
-  let e ←
-    Clap.Poseidon.poseidon
-      elems
-      (liftArr C)
-      (liftArr S)
-      (liftMat M)
-      (liftMat P)
-  pure e
+  Clap.Poseidon.poseidonBN254 elems
 
 end HashToField
 
