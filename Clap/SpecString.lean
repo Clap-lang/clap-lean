0c4697b76c11b1f8293cf21bf5617d19781dc471
Marco Stronati <marco@stronati.org>
2026-03-09 15:22:28 +0100

    ifthenelse thoughts

--- Clap/SpecString.lean
+++ Clap/SpecString.lean
@@ -15,11 +15,46 @@ info: abstractCore
 #guard_msgs in
 #synth Core p
 
+def g := 0
+def l := 5
+def r := 7
+
+#eval (g*l) + (1-g)*r
+
+open Clap.Lang.ZMod
+
+-- ite : {α : Sort u} → (c : Prop) → [h : Decidable c] → α → α → α
+
+-- conditional swap
+def ite (guard: FB p) (then_: F p) (else_ : F p) : F p :=
+  convert guard * then_ + (1 - convert guard) * else_
+
+--def ite (c : Prop) [h : Decidable c] (t e : F p) : F p := t
+
+abbrev pb := Primes.babybear
+
+instance : Coe (Core.FB pb) Prop := ⟨(· = FB.true)⟩
+
+example (b : FB pb): F pb := if b then const 1 else const 0
+
+-- we need to define new syntax, there is no way to overload the exusting mechanism
+
+
+-- #check decidable_of_iff
+
 def countZeros {maxLen : ℕ} (fs : Vector (F p) maxLen) : Option (F p) := do
-  Vector.foldlM (fun (len:F p) f => do
-    let b := F.eq f (const 0)
-    some (len + convert b)
+  Vector.foldlM (fun len f => do
+    ite (F.eq f (const 0))
+      (len + const 1)
+      len
   ) (const 0) fs
+
+
+-- def countZeros {maxLen : ℕ} (fs : Vector (F p) maxLen) : Option (F p) := do
+--   Vector.foldlM (fun (len:F p) f => do
+--     let b <- F.eq f (const 0)
+--     some (len + convert b)
+--   ) (const 0) fs
 
 /--
   Zero-padded vector of bytes of length `len`.
