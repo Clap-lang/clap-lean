3520bba77ca2391907462c1248d209b3a2288582
Marco Stronati <marco@stronati.org>
2026-03-09 10:57:10 +0100

    UNDO Share: remove share

--- Clap/Sha2/Circuit.lean
+++ Clap/Sha2/Circuit.lean
@@ -46,7 +46,7 @@ out = x*(y + z - 2*yz) + yz
 -/
 def maj (x y z : F32 p) : F32 p :=
   List.map (fun ((x,y),z) =>
-    let yz := shareB (y * z)
+    let yz := (y * z)
     x * (y + z - 2 * yz) + yz)
   ((x.zip y).zip z)
 
@@ -61,7 +61,7 @@ out = x * (1 - 2*y - 2*z + 4*yz) + y + z - 2 * yz
 -/
 def xor3 (x y z : F32 p) : F32 p :=
   List.map (fun ((x,y),z) =>
-    let yz := shareB (y * z)
+    let yz := (y * z)
     x * (1 - 2 * y - 2 * z + 4 * yz) + y + z - 2 * yz)
   ((x.zip y).zip z)
 
