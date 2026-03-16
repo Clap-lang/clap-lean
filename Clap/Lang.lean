30cbcc658f88448be6a785052fb8c76ad0f723c4
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2026-03-16 17:48:11 +0100

    Lang: remove extended class, just use reducible

--- Clap/Lang.lean
+++ Clap/Lang.lean
@@ -16,6 +16,7 @@ class Core (p : ℕ) : Type _ where
 
   [onlyForDebugF  : ToString F  ]
 
+attribute [reducible] Core.F Core.instF Core.const Core.accept Core.eq0 Core.share Core.isZero Core.num2bits Core.bits2num Core.onlyForDebugF
 attribute [instance] Core.instF Core.onlyForDebugF
 
 variable {p : ℕ} [Core p]
@@ -221,7 +222,7 @@ instance onlyForDebugF {p:ℕ} : ToString (ZMod p) where
   circuit, never while writing it. The risk is that a circuit which
   breaks the abstraction of Core won't be compilable.
 -/
-scoped instance instCoreZMod (p:ℕ) [Fact (Nat.Prime p)] : Core p where
+scoped instance instCoreZMod {p:ℕ} [Fact (Nat.Prime p)] : Core p where
   F := ZMod p
   const := id
   accept := Compiler.accept
@@ -231,24 +232,6 @@ scoped instance instCoreZMod (p:ℕ) [Fact (Nat.Prime p)] : Core p where
   num2bits := Compiler.num2bits
   bits2num := Compiler.bits2num
   onlyForDebugF
-
-
-/-
-TODO it should be possible to replace the extended definition below with this definition but there is an error
-class Extended (p:ℕ) [Fact (Nat.Prime p)] : Type _ extends Core p, DecidableEq (Core.F p)
--/
-
-class extended (p:ℕ) [Fact (Nat.Prime p)] [Core p] : Type _ where
-  ins : Core p
-  [i₀ : DecidableEq (Core.F p)]
-  [i₁ : {n:ℕ} → OfNat (Core.F p) n]
-
-attribute [instance] extended.i₀ extended.i₁
-
-scoped instance bla (p:ℕ) [Fact (Nat.Prime p)] : extended p where
-  ins := instCoreZMod p
-  i₀ := inferInstanceAs (DecidableEq (ZMod p))
-  i₁ := inferInstanceAs ({n:ℕ} → OfNat (ZMod p) n)
 
 end ZMod
 
@@ -260,23 +243,23 @@ abbrev p := Primes.goldilocks
 
 open Clap.Lang Core ZMod
 
-example : F.lessThan (p := p) 1 0 1 == some 1 := by native_decide
-example : F.lessThan (p := p) 1 0 0 == some 0 := by native_decide
-example : F.lessThan (p := p) 2 1 2 == some 1 := by native_decide
-example : F.lessThan (p := p) 2 2 1 == some 0 := by native_decide
-example : F.lessThan (p := p) 8 42 (2^8 - 1) == some 1 := by native_decide
-example : F.lessThan (p := p) 8 (2^8 - 1) 42 == some 0 := by native_decide
+example : F.lessThan 1 (0 : F p) 1 == some 1 := by native_decide
+example : F.lessThan 1 (0 : F p) 0 == some 0 := by native_decide
+example : F.lessThan 2 (1 : F p) 2 == some 1 := by native_decide
+example : F.lessThan 2 (2 : F p) 1 == some 0 := by native_decide
+example : F.lessThan 8 (42 : F p) (2^8 - 1) == some 1 := by native_decide
+example : F.lessThan 8 (2^8 - 1) (42 : F p) == some 0 := by native_decide
 
-example : F.lessEqThan (p := p) 2 2 2 == some 1 := by native_decide
-example : F.lessEqThan (p := p) 2 1 2 == some 1 := by native_decide
-example : F.lessEqThan (p := p) 2 3 2 == some 0 := by native_decide
+example : F.lessEqThan 2 (2 : F p) 2 == some 1 := by native_decide
+example : F.lessEqThan 2 (1 : F p) 2 == some 1 := by native_decide
+example : F.lessEqThan 2 (3 : F p) 2 == some 0 := by native_decide
 
-example : F.greaterThan (p := p) 2 3 2 == some 1 := by native_decide
-example : F.greaterThan (p := p) 2 2 2 == some 0 := by native_decide
+example : F.greaterThan 2 (3 : F p) 2 == some 1 := by native_decide
+example : F.greaterThan 2 (2 : F p) 2 == some 0 := by native_decide
 
-example : F.greaterEqThan (p := p) 2 3 2 == some 1 := by native_decide
-example : F.greaterEqThan (p := p) 2 2 2 == some 1 := by native_decide
-example : F.greaterEqThan (p := p) 2 2 3 == some 0 := by native_decide
+example : F.greaterEqThan 2 (3 : F p) 2 == some 1 := by native_decide
+example : F.greaterEqThan 2 (2 : F p) 2 == some 1 := by native_decide
+example : F.greaterEqThan 2 (2 : F p) 3 == some 0 := by native_decide
 
 
 def testBinSum (a b expected : FBitVec p) : Option Unit := do
