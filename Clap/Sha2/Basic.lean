5551d85ec75f79f366aee06ed38dca96f15751f0
Andrei Burdusa <andrei.burdusa@zoho.com>
2026-03-31 13:39:50 +0300

    Sha: use generic monad

--- Clap/Sha2/Basic.lean
+++ Clap/Sha2/Basic.lean
@@ -20,14 +20,14 @@ structure T.{u} : Type (u+1) where
   U8  : Type u
   U32 : Type u
 
-class Sha (t : T) where
-  [i₁ : Coe Nat t.U32]
-  [i₄ : Coe Nat t.U8]
-  [i₂ : Coe t.U8 t.U32]
-  [i₃ : Inhabited t.U32]
-  [i₇ : ToString t.U32]
-  [i₈ : ToString t.U8]
-  add32 : t.U32 → t.U32 → Option t.U32
+class Sha (m : Type → Type) [Monad m] (t : T) where
+  [i₁ : Coe Nat t.U8]
+  [i₂ : Coe Nat t.U32]
+  [i₃ : Coe t.U8 t.U32]
+  [i₄ : Inhabited t.U32]
+  [i₅ : ToString t.U8]
+  [i₆ : ToString t.U32]
+  add32 : t.U32 → t.U32 → m t.U32
   to_nat_be : Array t.U8 -> t.U32
   rotR       : Nat -> t.U32 -> t.U32
   shiftRight : Nat -> t.U32 -> t.U32
@@ -35,60 +35,71 @@ class Sha (t : T) where
   ch   : (x y z : t.U32) -> t.U32
   maj  : (x y z : t.U32) -> t.U32
 
-attribute [reducible] Sha.i₁ Sha.i₂ Sha.i₃ Sha.i₄ Sha.i₇ Sha.i₈
-attribute [instance] Sha.i₁ Sha.i₂ Sha.i₃ Sha.i₄ Sha.i₇ Sha.i₈
+attribute [reducible] Sha.i₁ Sha.i₂ Sha.i₃ Sha.i₄ Sha.i₅ Sha.i₆
+attribute [instance] Sha.i₁ Sha.i₂ Sha.i₃ Sha.i₄ Sha.i₅ Sha.i₆
 
-variable {t : T} [Sha t]
+variable (m : Type → Type) [Monad m]
+variable {t : T} [Sha m t]
 
 open Sha
 
-def of_nat_be (x:Nat) (len:Nat) : Array (t.U8) :=
-    (List.reverse (aux x len)).toArray
+def of_nat_be (x:Nat) (len:Nat) : Array t.U8 :=
+  (List.reverse (aux x len)).toArray
   where
     aux (x:Nat) (len:Nat) : List t.U8 :=
       let d : Nat := x / (2^8)
       let r : Nat := x % (2^8)
       let r : t.U8 := r -- does not wrap as r < 256
       if len=0 then [] else
-      r::(aux d (len-1))
+        r :: (aux d (len-1))
 
 def sigma_constants : Array Nat := #[7, 18, 3, 17, 19, 10]
 
 def sigma (c0 c1 c2 : Nat) (x : t.U32) : t.U32 :=
-  Sha.xor3 (Sha.rotR c0 x)
-           (Sha.rotR c1 x)
-           (Sha.shiftRight c2 x)
+  Sha.xor3 m
+    (Sha.rotR m c0 x)
+    (Sha.rotR m c1 x)
+    (Sha.shiftRight m c2 x)
 
 -- Sigma_0(x) = ROTR^{d0}(x) XOR ROTR^{d1}(x) XOR SHR^{d2}(x)
 def sigma_0 (x : t.U32) : t.U32 :=
-  sigma sigma_constants[0]!
-        sigma_constants[1]!
-        sigma_constants[2]! x
+  sigma m
+    sigma_constants[0]!
+    sigma_constants[1]!
+    sigma_constants[2]!
+    x
 
 -- Sigma_1(x) = ROTR^{d3}(x) XOR ROTR^{d4}(x) XOR SHR^{d5}(x)
 def sigma_1 (x : t.U32) : t.U32 :=
-  sigma sigma_constants[3]!
-        sigma_constants[4]!
-        sigma_constants[5]! x
+  sigma m
+    sigma_constants[3]!
+    sigma_constants[4]!
+    sigma_constants[5]!
+    x
 
 def sum_constants : Array Nat := #[2, 13, 22, 6, 11, 25]
 
 def sum (c0 c1 c2 : Nat) (x : t.U32) : t.U32 :=
-  Sha.xor3 (Sha.rotR c0 x)
-           (Sha.rotR c1 x)
-           (Sha.rotR c2 x)
+  Sha.xor3 m
+    (Sha.rotR m c0 x)
+    (Sha.rotR m c1 x)
+    (Sha.rotR m c2 x)
 
 -- Sum_0(x) = ROTR^{c0}(x) XOR ROTR^{c1}(x) XOR ROTR^{c2}(x)
 def sum_0 (x : t.U32) : t.U32 :=
-  sum sum_constants[0]!
-      sum_constants[1]!
-      sum_constants[2]! x
+  sum m
+    sum_constants[0]!
+    sum_constants[1]!
+    sum_constants[2]!
+    x
 
 -- Sum_1(x) = ROTR^{c3}(x) XOR ROTR^{c4}(x) XOR ROTR^{c5}(x)
 def sum_1 (x : t.U32) : t.U32 :=
-  sum sum_constants[3]!
-      sum_constants[4]!
-      sum_constants[5]! x
+  sum m
+    sum_constants[3]!
+    sum_constants[4]!
+    sum_constants[5]!
+    x
 
 def BLOCK_SIZE : Nat := 16
 def LEN_SIZE : Nat := 8
@@ -100,7 +111,8 @@ abbrev RoundConstantsTable (u32 : Type) : Type := Array u32 -- K_SIZE
 abbrev Block (u32 : Type) : Type := Array u32 -- BLOck_size
 abbrev Hash  (u32 : Type) : Type := Array u32 -- LEN_SIZE
 
-def round_constants_224_256 : RoundConstantsTable Nat :=
+def round_constants_224_256 : RoundConstantsTable t.U32 :=
+  Array.map Coe.coe
   #[0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
     0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
     0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
@@ -112,14 +124,14 @@ def round_constants_224_256 : RoundConstantsTable Nat :=
 
 def initial_hash : Hash t.U32 :=
   Array.map Coe.coe
-   #[0x6a09e667,
-     0xbb67ae85,
-     0x3c6ef372,
-     0xa54ff53a,
-     0x510e527f,
-     0x9b05688c,
-     0x1f83d9ab,
-     0x5be0cd19]
+  #[0x6a09e667,
+    0xbb67ae85,
+    0x3c6ef372,
+    0xa54ff53a,
+    0x510e527f,
+    0x9b05688c,
+    0x1f83d9ab,
+    0x5be0cd19]
 
 -- Section 5.1.1
 -- #[hax_lib::requires((msg.len() as u64) < 0x1fffffffffffffff)]
@@ -129,7 +141,7 @@ def padding (msg : Array t.U8) : Array t.U8 :=
     let n_zero_bytes := 64 - l
     Array.replicate n_zero_bytes (Coe.coe 0)
   let k_one_byte : t.U8 := Coe.coe 128 -- one byte with a single 1 as msb
-  let l := of_nat_be (msg.size*8) 8
+  let l := of_nat_be m (msg.size*8) 8
   msg ++ #[k_one_byte] ++ k_zero_bytes ++ l
 
 -- result size 16
@@ -138,7 +150,7 @@ def parse_u32 (msg : Array t.U8) : Array t.U32 :=
   aux 0 #[]
 where
   aux (start:Nat) (acc : Array t.U32) : Array t.U32 :=
-    let u32 : t.U32 := Sha.to_nat_be (Array.extract msg start (stop:=start+4))
+    let u32 : t.U32 := Sha.to_nat_be m (Array.extract msg start (stop:=start+4))
     let acc := acc.push u32
     if start >= msg.size-4
     then assert! (acc.size = 16) ; acc
@@ -151,29 +163,29 @@ def parse_blocks (msg : Array t.U8) : Array (Block t.U32) :=
 where
   aux (start : Nat) (acc : Array (Block t.U32)) : Array (Block t.U32) :=
     let block : Array t.U8 := Array.extract msg start (stop:=start+64)
-    let acc := acc.push (parse_u32 block)
+    let acc := acc.push (parse_u32 m block)
     if start >= msg.size-64
     then acc
     else aux (start+64) acc
 
 -- Section 6.2.2 step 1
-def schedule (block : Block t.U32) : Option (RoundConstantsTable t.U32) := do
+def schedule (block : Block t.U32) : m (RoundConstantsTable t.U32) :=
   aux block 16
 where
-  aux (acc : Array t.U32) (i:Nat) : Option (Array t.U32) := do
-    if i >= K_SIZE then acc else
+  aux (acc : Array t.U32) (i:Nat) : m (Array t.U32) := do
+    if i >= K_SIZE then return acc else
     let t16 := acc[i - 16]!
     let t15 := acc[i - 15]!
     let t7 := acc[i - 7]!
     let t2 := acc[i - 2]!
 
-    let sum ← [sigma_1 t2, t7, sigma_0 t15, t16].foldlM add32 default
+    let sum ← [sigma_1 m t2, t7, sigma_0 m t15, t16].foldlM add32 default
     let acc := acc.push sum
 
     aux acc (i+1)
 
 -- Section 6.2.2 step 3
-def shuffle_i (ws:RoundConstantsTable t.U32) (hash: Hash t.U32) (i:Nat) : Option (Hash t.U32) := do
+def shuffle_i (ws:RoundConstantsTable t.U32) (hash: Hash t.U32) (i:Nat) : m (Hash t.U32) := do
 
   let a := hash[0]!
   let b := hash[1]!
@@ -184,8 +196,8 @@ def shuffle_i (ws:RoundConstantsTable t.U32) (hash: Hash t.U32) (i:Nat) : Option
   let g := hash[6]!
   let h := hash[7]!
 
-  let t1 ← [h, sum_1 e, Sha.ch e f g, round_constants_224_256[i]!, ws[i]!].foldlM add32 default
-  let t2 ← [sum_0 a, Sha.maj a b c].foldlM add32 default
+  let t1 ← [h, sum_1 m e, Sha.ch m e f g, (round_constants_224_256 m)[i]!, ws[i]!].foldlM add32 default
+  let t2 ← [sum_0 m a, Sha.maj m a b c].foldlM add32 default
 
   let h := g
   let g := f
@@ -196,31 +208,31 @@ def shuffle_i (ws:RoundConstantsTable t.U32) (hash: Hash t.U32) (i:Nat) : Option
   let b := a
   let a ← add32 t1 t2
 
-  #[a, b, c, d, e, f, g, h]
+  return #[a, b, c, d, e, f, g, h]
 
-def shuffle (ws:RoundConstantsTable t.U32) (hash: Hash t.U32) : Option (Hash t.U32) :=
+def shuffle (ws:RoundConstantsTable t.U32) (hash: Hash t.U32) : m (Hash t.U32) :=
   aux hash 0
 where
-  aux hash (i:Nat) : Option (Hash t.U32) := do
+  aux hash (i:Nat) := do
     if i>=64 then pure hash else
-    let hash ← shuffle_i ws hash i
+    let hash ← shuffle_i m ws hash i
     aux hash (i+1)
 
-def compress (block : Block t.U32) (hash : Hash t.U32) : Option (Hash t.U32) := do
-  let ws ← schedule block
-  let hash' ← shuffle ws hash
+def compress (block : Block t.U32) (hash : Hash t.U32) : m (Hash t.U32) := do
+  let ws ← schedule m block
+  let hash' ← shuffle m ws hash
   -- Section 6.2.2 step 4
   Array.zipWithM add32 hash' hash
 
 -- TODO this should return Sha256digest
-def digest (msg : Array t.U8) : Option (Hash t.U32) :=
-  let blocks := padding msg
-  let blocks := parse_blocks blocks
-  process_blocks blocks initial_hash 0
+def digest (msg : Array t.U8) : m (Hash t.U32) :=
+  let blocks := padding m msg
+  let blocks := parse_blocks m blocks
+  process_blocks blocks (initial_hash m) 0
 where
   process_blocks (blocks : Array (Block t.U32)) (acc : Hash t.U32) i := do
-    if i >= blocks.size then acc else
-    let acc ← compress blocks[i]! acc
+    if i >= blocks.size then pure acc else
+    let acc ← compress m blocks[i]! acc
     process_blocks blocks acc (i+1)
 
 end Clap.Sha2
--- Clap/Sha2/Test.lean
+++ Clap/Sha2/Test.lean
@@ -8,21 +8,21 @@ open Clap.Sha2
 
 def stringToU8s (s:String) : Array UInt8 := s.toUTF8.data
 
-example : digest (t := Clap.Sha2.Cpu.t) (stringToU8s "abc") =
-  some #[0xba7816bf, 0x8f01cfea, 0x414140de, 0x5dae2223, 0xb00361a3, 0x96177a9c, 0xb410ff61, 0xf20015ad] := by
+example : Id.run (digest Id (t := Clap.Sha2.Cpu.t) (stringToU8s "abc")) =
+  #[0xba7816bf, 0x8f01cfea, 0x414140de, 0x5dae2223, 0xb00361a3, 0x96177a9c, 0xb410ff61, 0xf20015ad] := by
   native_decide
 
-example : digest (t := Clap.Sha2.Cpu.t) (stringToU8s "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq") =
-  some #[0x248d6a61, 0xd20638b8, 0xe5c02693, 0x0c3e6039, 0xa33ce459, 0x64ff2167, 0xf6ecedd4, 0x19db06c1] := by
+example : Id.run (digest Id (t := Clap.Sha2.Cpu.t) (stringToU8s "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq")) =
+  #[0x248d6a61, 0xd20638b8, 0xe5c02693, 0x0c3e6039, 0xa33ce459, 0x64ff2167, 0xf6ecedd4, 0x19db06c1] := by
   native_decide
 
 end TestCpu
-/-
+
 namespace TestCircuit
 
 abbrev p := Primes.goldilocks
 
-open Clap.Sha2
+open Clap.Sha2 Circuit
 open Clap.Lang Core ZMod
 
 def stringToU8s (s:String) : Array (F8 p) :=
@@ -31,10 +31,14 @@ def stringToU8s (s:String) : Array (F8 p) :=
     let b : F p := (b.toNat : F p)
     F8.ofF! b
 
-example : digest (t := Clap.Sha2.Circuit.t p) (stringToU8s "abc") = #[0xba7816bf, 0x8f01cfea, 0x414140de, 0x5dae2223, 0xb00361a3, 0x96177a9c, 0xb410ff61, 0xf20015ad] := by native_decide
+private instance : Sha Option (t p) := Clap.Sha2.Circuit.instShaOptionT
 
-example : digest (t := Clap.Sha2.Circuit.t p) (stringToU8s "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq") =
-   #[0x248d6a61, 0xd20638b8, 0xe5c02693, 0x0c3e6039, 0xa33ce459, 0x64ff2167, 0xf6ecedd4, 0x19db06c1] := by native_decide
+example : digest Option (t := Clap.Sha2.Circuit.t p) (stringToU8s "abc") =
+  some #[0xba7816bf, 0x8f01cfea, 0x414140de, 0x5dae2223, 0xb00361a3, 0x96177a9c, 0xb410ff61, 0xf20015ad] := by
+  native_decide
+
+example : digest Option (t := Clap.Sha2.Circuit.t p) (stringToU8s "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq") =
+  some #[0x248d6a61, 0xd20638b8, 0xe5c02693, 0x0c3e6039, 0xa33ce459, 0x64ff2167, 0xf6ecedd4, 0x19db06c1] := by
+  native_decide
 
 end TestCircuit
--/
