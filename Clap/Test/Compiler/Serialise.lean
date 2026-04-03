b34db6e9054efb19e151f9c58984a7505eb3c4a0
Marco Stronati <marco@stronati.org>
2026-04-03 16:47:12 +0200

    compiler test normalize

--- Clap/Test/Compiler/Serialise.lean
+++ Clap/Test/Compiler/Serialise.lean
@@ -8,7 +8,7 @@ namespace Test
 
 namespace Compiler
 
-open Lean Clap.Compiler Meta
+open Lean Clap.Compiler Meta Lang
 
 structure Point2 (p : Nat) where
   x : ZMod p
@@ -19,16 +19,16 @@ structure Point3 (p : Nat) where
   y : ZMod p
   z : ZMod p
 
-def Serialise.ex₁ (p₁ : Point2 Primes.babybear) (p₂ : Point3 Primes.babybear) : Option Unit := do
+def Serialise.ex₁ {p} [Core p] (p₁ : Point2 p) (p₂ : Point3 p) : Option Unit := do
   Spec.Compiler.eq0 (p₁.x + p₂.z)
   Spec.Compiler.accept
 
 /--
-info: def - fun (p₁_circuit : Vector (ZMod Primes.babybear) 2) (p₂_circuit : Vector (ZMod Primes.babybear) 3) => do
+info: def - fun {p : ℕ} (p₁_circuit : Vector (ZMod Primes.babybear) 2) (p₂_circuit : Vector (ZMod Primes.babybear) 3) => do
   Spec.Compiler.eq0 (p₁_circuit[0] + p₂_circuit[2])
   some Spec.Compiler.accept
 ---
-info: type - Vector (ZMod Primes.babybear) 2 → Vector (ZMod Primes.babybear) 3 → Option Unit
+info: type - {p : ℕ} → Vector (ZMod Primes.babybear) 2 → Vector (ZMod Primes.babybear) 3 → Option Unit
 -/
 #guard_msgs(info, whitespace := lax) in
 set_option pp.funBinderTypes true in
