426b16b14f8747eb8d7cb1f6e16e813ba0d70cd3
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2026-04-08 13:35:47 +0200

    cleanup cimplol

--- Clap/Compiler/Cimplol.lean
+++ Clap/Compiler/Cimplol.lean
@@ -5,7 +5,11 @@ import Clap.Compiler.Wheels
 
 namespace Clap.Compiler
 
-open Lean in
+open Lean
+
+private def originalIdentifier (ident : Name) : Name := 
+  ident.updateLast fun name ↦ String.intercalate "_" (name.splitOn "_").dropLast
+
 elab "cimplol" "(" f:ident ", " p:ident ", " simpset:ident ")" : term => do
   let [f] ← realizeGlobalConst f | throwError m!"Undeclared constant:\n{f}"
   trace[Clap.Compiler.preprocess] m!"Resolved into: {f}"
@@ -17,16 +21,15 @@ elab "cimplol" "(" f:ident ", " p:ident ", " simpset:ident ")" : term => do
     | .some f => return f.value!
   trace[Clap.Compiler.preprocess] m!"declName := {f}\np := {p.getId}\nn := 2048\narg := {simpset.getId}"
   let (compiledF, _) ←
+    -- Lean assumes that `def f (n : ℕ) : α := ?_` is recursive and injects
+    -- `f : ℕ → α` as a free variable for `?_`, which one has to account for.
     Meta.withErasedFVars #[
-      (←getLCtx).findFromUserName? (
-        f.updateLast fun name ↦ String.intercalate "_" (name.splitOn "_").dropLast
-      ) |>.get!.fvarId
-    ] do
-      compileMeta (declName := f)
-                  (p := p.getId)
-                  (n := 2048)
-                  (σ := {})
-                  (arg := simpset.getId)
+      (←getLCtx).findFromUserName? (originalIdentifier f) |>.get!.fvarId
+    ] do compileMeta (declName := f)
+                     (p := p.getId)
+                     (n := 2048)
+                     (σ := {})
+                     (arg := simpset.getId)
   return compiledF
 
 end Clap.Compiler
