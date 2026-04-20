ae9da074e54d6d4410c6872e46c72ad7eb17a891
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2026-04-20 18:25:55 +0200

    Compiler: incorporate reduction in traversal

--- Clap/Compiler/Traverse.lean
+++ Clap/Compiler/Traverse.lean
@@ -8,7 +8,12 @@ namespace Clap.Compiler
 
 open Lean Meta Qq Elab
 
-abbrev ExprS := Sum Expr Expr
+abbrev ExprS := Expr × Expr ⊕ Expr
+
+def ExprS.pretty (e : ExprS) : MetaM String := do
+  match e with
+  | .inl (e, binder) => return s!"λ {binder} ↦ {←PrettyPrinter.ppExpr e}"
+  | .inr e => PrettyPrinter.ppExpr e <&> Format.pretty
 
 /--
 TODO: We will probably remove the old compiler.
@@ -22,51 +27,61 @@ TODO: We will probably remove the old compiler.
 This is based on `getBindArgs!`.
 -/
 def _root_.Lean.Expr.getBindArgs? (e : Expr) : MetaM (Option (Expr × Expr)) := do
-  if !(←e.isBind) then return .none
-  let firstExplicitArg := (←getFunInfo e.getAppFn).paramInfo.findIdx (·.binderInfo.isExplicit)
-  let args := e.getAppArgs
-  return (args[firstExplicitArg]!, args[firstExplicitArg + 1]!)
+  -- If `e` is not `λ _ ↦ _`, then `lambdaTelescope = id`.
+  lambdaTelescope e fun _ e ↦ do
+    if !(←e.isBind) then return .none
+    let firstExplicitArg := (←getFunInfo e.getAppFn).paramInfo.findIdx (·.binderInfo.isExplicit)
+    let bindArgs := e.getAppArgs
+    return .some (
+      bindArgs[firstExplicitArg]!,
+      bindArgs[firstExplicitArg + 1]!
+    )
 
-def _root_.Lean.Expr.mkBind (l r : Expr) (m? : Name := ``Bind.bind) : MetaM Expr :=
+def _root_.Lean.Expr.mkBind (l r : Expr) (m? : Name := ``Bind.bind) : MetaM Expr := do
   mkAppM m? #[l, r]
 
 private def treeEmoji : String := "🌲"
 
 mutual
 
-private partial def down (todo : Expr) (stack : List ExprS) : TermElabM Expr := do
+private partial def down (reduce : Expr → TermElabM Expr)
+                         (todo : Expr) (stack : List ExprS) : TermElabM Expr := do
   if let .some (l, r) ← todo.getBindArgs?
   then
     trace[Clap.Compile.down] "\npush [→]:\n{r}\ngo [↓]:\n{l}"
-    down l (.inr r :: stack)
+    down reduce l (.inr r :: stack)
   else
-    let simped ← Simp.simplify `dbgSimp todo
+    let simped ← reduce todo
     trace[Clap.Compile.simp] "\n{todo}\n--->\n{simped}"
     if simped != todo
     then
       trace[Clap.Compile.down] "\n{checkEmoji} go [↓]:\n{simped}"
-      down simped stack
+      down reduce simped stack
     else
       trace[Clap.Compile.down] "\n{crossEmoji} go [↑]:\n{todo}"
-      up todo stack
+      up reduce todo stack 
 
-private partial def up (done : Expr) (stack : List ExprS) : TermElabM Expr := do
+private partial def up (reduce : Expr → TermElabM Expr)
+                       (done : Expr) (stack : List ExprS) : TermElabM Expr := do
   match stack with
   | [] =>
     trace[Clap.Compile.up] "Done"
     return done
   | .inr r :: stack =>
-    trace[Clap.Compile.up] "\npush [←]:\n{done}\ngo [↓]:\n{r}"
-    down r (.inl done :: stack)
+    lambdaTelescope r fun args body ↦ do
+      let #[arg] := args | unreachable!
+      trace[Clap.Compile.up] "\npush [←]:\n{done}\ngo [↓]:\n{r}"
+      down reduce body (.inl (done, arg) :: stack)
   | .inl l :: stack => do
-    trace[Clap.Compile.up] "\ngo [↑]:\n{l}\n>>=\n{done}"
-    up (←Expr.mkBind l done) stack
+    let lam ← mkLambdaFVars #[l.2] done
+    trace[Clap.Compile.up] "\ngo [↑]:\n{l}\n>>=\n{lam}"
+    up reduce (←Expr.mkBind l.1 lam) stack
 
 end
 
-def compile (e : Expr) : TermElabM Expr := do
+def compile (e : Expr) (simpset : Name := `simpAll) : TermElabM Expr := do
   withTraceNode `Clap.Compile formatExprWith do
-  down e []
+  down (Simp.simplify simpset) e []
 
 namespace Exampru
 
@@ -75,19 +90,22 @@ opaque eq0 (e : Unit) : Option Unit
 def ex₀ : Expr := q(
   do eq0 ()
      eq0 ()
-     let res ← (do eq0 (); eq0 ())
+     let res ← ([0, 1].foldlM (init := ()) fun _ _ ↦ eq0 ())
      eq0 res
      return ()
-)
-
-attribute [dbgSimp] Option.bind_eq_bind
-
--- set_option trace.Clap.Compile true
+)                    
 
 /--
 info: do
   eq0 ()
-  (eq0 ()).bind fun x => ((eq0 ()).bind fun x => eq0 ()).bind fun res => (eq0 res).bind fun x => pure ()
+  eq0 ()
+  let res ←
+    do
+      eq0 ()
+      let init ← eq0 ()
+      some init
+  eq0 res
+  some ()
 -/
 #guard_msgs in
 #eval compile ex₀ >>= (liftM ∘ PrettyPrinter.ppExpr)
