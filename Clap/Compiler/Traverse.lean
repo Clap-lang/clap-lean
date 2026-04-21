380fe5a06fd00550b7e4eac6ecea4616e7b9a00b
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2026-04-21 18:47:57 +0200

    Compiler: structural compilation, simp interface, tracing

--- Clap/Compiler/Traverse.lean
+++ Clap/Compiler/Traverse.lean
@@ -15,17 +15,9 @@ def ExprS.pretty (e : ExprS) : MetaM String := do
   | .inl (e, binder) => return s!"λ {binder} ↦ {←PrettyPrinter.ppExpr e}"
   | .inr e => PrettyPrinter.ppExpr e <&> Format.pretty
 
-/--
-TODO: We will probably remove the old compiler.
-This is `isBind`.
--/
-def _root_.Lean.Expr.isBind (e : Expr) : MetaM Bool :=
-  isDefEq e.getAppFn (.const ``Bind.bind [0, 0])
+def _root_.Lean.Expr.isBind (e : Expr) : MetaM Bool := do
+  return e.isAppOf ``Bind.bind || e.isAppOf ``Option.bind
 
-/--
-TODO: We will probably remove the old compiler.
-This is based on `getBindArgs!`.
--/
 def _root_.Lean.Expr.getBindArgs? (e : Expr) : MetaM (Option (Expr × Expr)) := do
   -- If `e` is not `λ _ ↦ _`, then `lambdaTelescope = id`.
   lambdaTelescope e fun _ e ↦ do
@@ -48,32 +40,32 @@ def _root_.Lean.Meta.lambdaTelescopeOne!.{u}
     let #[arg] := args | panic! "Expected a single argument. Got: {args.size}"
     k arg body
 
-def mkBindWith (stackEntry : Expr × Expr) (cont : Expr) (m? : Name := ``Bind.bind) : MetaM Expr := do
-  mkLambdaFVars #[stackEntry.2] cont >>= stackEntry.1.mkBind (m? := m?)
-
 private def treeEmoji : String := "🌲"
 
 mutual
 
 private partial def down (reduce : Expr → TermElabM Expr)
-                         (todo : Expr) (stack : List ExprS) : TermElabM Expr := do
+                         (reduceOuter : Expr → TermElabM Expr)
+                         (stack : List ExprS) (todo : Expr) : TermElabM Expr := do
   if let .some (l, r) ← todo.getBindArgs?
   then
     trace[Clap.Compile.down] "\npush [→]:\n{r}\ngo [↓]:\n{l}"
-    down reduce l (.inr r :: stack)
+    down reduce reduceOuter (.inr r :: stack) l
   else
     let simped ← reduce todo
-    trace[Clap.Compile.simp] "\n{todo}\n--->\n{simped}"
     if simped != todo
     then
-      trace[Clap.Compile.down] "\n{checkEmoji} go [↓]:\n{simped}"
-      down reduce simped stack
+      trace[Clap.Compile.simp] "[↓] {checkEmoji}\n{todo}\n--->\n{simped}"
+      trace[Clap.Compile.down] "\ngo [↓]:\n{simped}"
+      down reduce reduceOuter stack simped
     else
-      trace[Clap.Compile.down] "\n{crossEmoji} go [↑]:\n{todo}"
-      up reduce todo stack 
+      trace[Clap.Compile.simp] "[↓] {crossEmoji}\n{todo}"
+      trace[Clap.Compile.down] "\ngo [↑]:\n{todo}"
+      up reduce reduceOuter stack todo
 
 private partial def up (reduce : Expr → TermElabM Expr)
-                       (done : Expr) (stack : List ExprS) : TermElabM Expr := do
+                       (reduceOuter : Expr → TermElabM Expr)
+                       (stack : List ExprS) (done : Expr) : TermElabM Expr := do
   match stack with
   | [] =>
     trace[Clap.Compile.up] "Done"
@@ -81,44 +73,70 @@ private partial def up (reduce : Expr → TermElabM Expr)
   | .inr r :: stack =>
     lambdaTelescopeOne! r fun arg body ↦ do
       trace[Clap.Compile.up] "\npush [←]:\n{(done, arg)}\ngo [↓]:\n{body}"
-      down reduce body (.inl (done, arg) :: stack)
+      down reduce reduceOuter (.inl (done, arg) :: stack) body
   | .inl l :: stack => do
     let bind ← mkBindWith l done
-    trace[Clap.Compile.up] "\ngo [↑]:\n{l}\n>>=\n{bind}"
-    up reduce bind stack
+    let up := up reduce reduceOuter stack
+    if ← isDefEq (←inferType l.2) q(Unit)
+    then trace[Clap.Compile.up] "\ngo [↑]:\n{bind}"
+         up bind
+    else trace[Clap.Compile.simp] "Binding value: {l.2}"
+
+         let simped ← reduceOuter bind
+         if simped != bind
+         then trace[Clap.Compile.simp] "[↑] {checkEmoji}\n{bind}\n--->\n{simped}"
+         else trace[Clap.Compile.simp] "[↑] {crossEmoji}\n{bind}"
+
+         trace[Clap.Compile.up] "\ngo [↑]:\n{simped}"
+         up simped
+  where mkBindWith (stackEntry : Expr × Expr) (cont : Expr)
+                   (m? : Name := ``Bind.bind) : MetaM Expr := do
+    mkLambdaFVars #[stackEntry.2] cont >>= stackEntry.1.mkBind (m? := m?)
 
 end
 
-def compile (e : Expr) (simpset : Name := `simpAll) : TermElabM Expr := do
+open Simp API
+
+def compile (e : Expr) (simpset : SimpSet) (only : Bool := true) : TermElabM Expr := do
   withTraceNode `Clap.Compile formatExprWith do
-  down (Simp.simplify simpset) e []
+  trace[Clap.Compile.simp.config]
+    m!"Reducer: [only := {only}, singlePass := {true}, set := {repr simpset}"
+  trace[Clap.Compile.simp.config]
+    m!"Compiler: [only := {false}, singlePass := {false}, set := {repr compilerSet} ∪ {repr simpset}"
+    
+  down (Simp.simplify (only := only) (singlePass := true) simpset)
+       (Simp.simplify (only := false) (singlePass := false) (compilerSet.union simpset)) [] e
+  where
+    compilerSet : SimpSet :=
+      SimpSet.withAllPost #[``Option.bind_assoc, ``bind_assoc] #[``Option.bind_eq_bind]
 
 namespace Exampru
 
-opaque eq0 (e : Unit) : Option Unit
+opaque eq0 (e : Nat) : Option Unit
 
 def ex₀ : Expr := q(
-  do eq0 ()
-     eq0 ()
-     let res ← ([0, 1].foldlM (init := ()) fun _ _ ↦ eq0 ())
-     eq0 res
+  do eq0 0
+     eq0 1
+     let res ← ([0, 1].foldlM (init := ()) fun _ _ ↦ eq0 2)
+     eq0 3
      return ()
 )                    
 
 /--
 info: do
-  eq0 ()
-  eq0 ()
-  let res ←
-    do
-      eq0 ()
-      let init ← eq0 ()
-      some init
-  eq0 res
-  some ()
+  eq0 0
+  eq0 1
+  do
+    eq0 2
+    let init ← eq0 2
+    pure init
+  eq0 3
+  pure ()
 -/
 #guard_msgs in
-#eval compile ex₀ >>= (liftM ∘ PrettyPrinter.ppExpr)
+#eval compile ex₀
+  (SimpSet.withAllPost #[``List.foldlM_cons, ``List.foldlM_nil]) >>=
+  (liftM ∘ PrettyPrinter.ppExpr)
 
 end Exampru
 
