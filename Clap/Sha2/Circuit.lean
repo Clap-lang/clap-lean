656308169fb9e042dc808203bee6e5e9aae91433
Marco Stronati <marco@stronati.org>
2026-05-17 09:41:33 +0200

    Sha: comment compilation test

--- Clap/Sha2/Circuit.lean
+++ Clap/Sha2/Circuit.lean
@@ -194,13 +194,13 @@ def test₁ {p:ℕ} [Core p] [Fact (Primes.fits p 32)]
   F32.assert_eq (Clap.Sha2.Circuit.ch x y z) F32.default
   -- accept p
 
-/--
-info: Compiled test₁ into test₁_circuit.
----
-info: Wg for test₁ is test₁_wg_wrap.
--/
-#guard_msgs in
-#compile test₁ using Primes.goldilocks
+-- /--
+-- info: Compiled test₁ into test₁_circuit.
+-- ---
+-- info: Wg for test₁ is test₁_wg_wrap.
+-- -/
+-- #guard_msgs in
+-- #compile test₁ using Primes.goldilocks
 
 end TestCompilation
 
