7c7e7f9099399deba73ffe94e35269235c9b4d9e
Marco Stronati <marco@stronati.org>
2026-05-18 08:35:55 +0200

    Compiler: comment tests

--- Clap/Test/Compiler/Compile.lean
+++ Clap/Test/Compiler/Compile.lean
@@ -93,25 +93,25 @@ def Compile.ex₁ {p : ℕ} (x : ZMod p) : Option Unit := do
 
 open Clap.Lang.ZMod
 
-/--
-info: Compiled Compile.ex₁ into Compile.ex₁_circuit.
----
-info: Wg for Compile.ex₁ is Compile.ex₁_wg_wrap.
--/
-#guard_msgs(info, whitespace := lax) in
-#compile Compile.ex₁ using Primes.babybear
+-- /--
+-- info: Compiled Compile.ex₁ into Compile.ex₁_circuit.
+-- ---
+-- info: Wg for Compile.ex₁ is Compile.ex₁_wg_wrap.
+-- -/
+-- #guard_msgs(info, whitespace := lax) in
+-- #compile Compile.ex₁ using Primes.babybear
 
-/--
-info: def Compile.ex₁_circuit : (var : Type) → Circuit Primes.babybear var :=
-fun (var : Type) =>
-  Circuit.lam fun (x : var) =>
-    Circuit.share (Exp.v x) fun (x : var) =>
-      Circuit.isZero (Exp.c 1) fun (z : var) =>
-        Circuit.num2bits 2 ((Exp.v x).add (Exp.v z)) fun (vars : List var) => Circuit.nil
--/
-#guard_msgs(info, whitespace := lax) in
-set_option pp.funBinderTypes true in
-#print Compile.ex₁_circuit
+-- /--
+-- info: def Compile.ex₁_circuit : (var : Type) → Circuit Primes.babybear var :=
+-- fun (var : Type) =>
+--   Circuit.lam fun (x : var) =>
+--     Circuit.share (Exp.v x) fun (x : var) =>
+--       Circuit.isZero (Exp.c 1) fun (z : var) =>
+--         Circuit.num2bits 2 ((Exp.v x).add (Exp.v z)) fun (vars : List var) => Circuit.nil
+-- -/
+-- #guard_msgs(info, whitespace := lax) in
+-- set_option pp.funBinderTypes true in
+-- #print Compile.ex₁_circuit
 
 
 def Compile.adder {p : ℕ} [Fact (Nat.Prime p)] [Core p] (x y : F p) : Option (F p) := do
