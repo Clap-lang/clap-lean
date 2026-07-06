9030b5232a3cc47763c7f8e3a9fde6375e71fdb9
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2026-07-06 19:58:45 +0200

    WIP

--- Clap/eDSL/Specification.lean
+++ Clap/eDSL/Specification.lean
@@ -5,15 +5,60 @@ namespace Clap
 
 namespace Spec
 
-def eval_expr {p: ℕ} {var: Type} (varStore : var → ZMod p) (e: Exp p var) : ZMod p := match e with
-  | .c x => x
-  | .v x => varStore x
-  | .add l r => eval_expr varStore l + eval_expr varStore r
-  | .sub l r => eval_expr varStore l - eval_expr varStore r
-  | .mul l r => eval_expr varStore l * eval_expr varStore r
+inductive Exp (p : ℕ) where
+  | v   (_ : ℕ)
+  | c   (_ : ZMod p)
+  | add (_ _ : Exp p )
+  | mul (_ _ : Exp p )
+  | sub (_ _ : Exp p )
+  deriving DecidableEq
 
-def eval {p: ℕ} {var: Type} (varStore : var → ZMod p) (circuit: Circuit p var) : denotation var :=
-  sorry
+inductive Circuit (p : ℕ) : Type where
+  | nil
+  | eq0 (e : Exp p) (c : Circuit p)
+  | lam (cont : ℕ → Circuit p)
+  | share (e : Exp p) (cont : ℕ → Circuit p)
+  | isZero (e : Exp p) (cont : ℕ → Circuit p)
+  | num2bits (w : ℕ) (e : Exp p) (cont : List ℕ → Circuit p)
+
+structure AllocatedCircuit (p : ℕ) where
+  varStore : Std.TreeMap ℕ (ZMod p)
+  circuit : Circuit p
+
+def isVarAllocated {p : ℕ} (numAlloc : ℕ) (e : Exp p) : Bool :=
+  match e with
+  | .v n => n < numAlloc
+  | _ => true
+
+-- @[aesop safe [constructors, cases]]
+-- inductive WFAllocatedCircuit (p : ℕ) : AllocatedCircuit p → Prop where
+--   | nil {numAlloc : ℕ} :
+--         WFAllocatedCircuit p ⟨numAlloc, .nil⟩
+--   | eq0 {numAlloc : ℕ} {c : Circuit p} {e : Exp p}
+--         (h : isVarAllocated numAlloc e)
+--         (rec : WFAllocatedCircuit p ⟨numAlloc, c⟩) :
+--         WFAllocatedCircuit p ⟨numAlloc, .eq0 e c⟩
+--   | lam {numAlloc : ℕ} {cont : ℕ → Circuit p}
+--         (rec : WFAllocatedCircuit p ⟨numAlloc.succ, cont numAlloc⟩) :
+--         WFAllocatedCircuit p ⟨numAlloc, .lam cont⟩
+--   -- | 
+
+-- open Circuit in
+-- def ex : Circuit 57 := .lam fun x ↦ .lam fun y ↦ .eq0 (.v 1) .nil
+
+-- open WFAllocatedCircuit in
+-- example : WFAllocatedCircuit 57 ⟨0, ex⟩ := by
+--   unfold ex
+  -- aesop (add simp ex)
+
+def eval_expr {p : ℕ} (varStore : Std.TreeMap ℕ (ZMod p)) (e : Exp p) : Option (ZMod p) := match e with
+  | .c x => .some x
+  | .v x => varStore.get? x
+  | .add l r => do (←eval_expr varStore l) + (←eval_expr varStore r)
+  | .sub l r => do (←eval_expr varStore l) - (←eval_expr varStore r)
+  | .mul l r => do (←eval_expr varStore l) * (←eval_expr varStore r)
+
+def eval {p: ℕ} (circuit : AllocatedCircuit p) : denotation (ZMod p) :=
 -- Defining parametric eval like this does not seem easy
 -- either the cont members in constructors like share and isZero need to be switched to exp and we
 -- make eval have them pass through a .c, then prove that that's equivalent to them making a new variable
@@ -22,21 +67,35 @@ def eval {p: ℕ} {var: Type} (varStore : var → ZMod p) (circuit: Circuit p va
 -- it could be making var a Fintype that keeps changing, or other options
 -- There may be far reaching changes required for either updating share, isZero etc to return exps, or for
 -- making additional claims about var here
--- match circuit with
---   | .nil =>
---       .u
---   | .lam k =>
---       .l fun x => eval varStore (k x)
---   | .eq0 e c =>
---       if e.eval = 0 then eval varStore c else .n
---   | .share e k =>
---       if ∃ x: var, varStore x = eval_expr varStore e ∧ eval varStore (k x) = .u
---       then .u
---       else .n
---   | .isZero e k =>
---       if e.eval = 0 then (k 1).eval else (k 0).eval
---   | .num2bits w e k =>
---       if e.eval.val < 2^w then (k (num2bitsLsbPure w e.eval)).eval else .n
+  let ⟨varStore, circuit⟩ := circuit
+  match circuit with
+  | .nil =>
+    .u
+  | .lam k =>
+    .l fun x =>
+    let newIdx := varStore.size
+    eval ⟨varStore.insert newIdx x, k newIdx⟩
+  | .eq0 e c =>
+    if eval_expr varStore e = .some 0 then eval ⟨varStore, c⟩ else .n
+  | .share e k =>
+    let newIdx := varStore.size
+    let val := eval_expr varStore e
+    match val with
+    | .some val => eval ⟨varStore.insert newIdx val, k newIdx⟩
+    | .none => .n
+  | .isZero e k =>
+    let invIdx := varStore.size
+    let oIdx := invIdx + 1
+    match eval_expr varStore e with
+    | .some val =>
+      let inv := val⁻¹
+      if val = 0
+      then eval ⟨varStore.insert invIdx inv |>.insert oIdx 1, k oIdx⟩
+      else eval ⟨varStore.insert invIdx inv |>.insert oIdx 0, k oIdx⟩
+    | .none =>
+      .n
+  | .num2bits w e k =>
+      if e.eval.val < 2^w then (k (num2bitsLsbPure w e.eval)).eval else .n
 
 def equiv {p: ℕ} {var: Type} {ResultT: Type}
   (varStore : var → ZMod p)
