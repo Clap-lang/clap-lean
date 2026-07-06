ce724c9ee222d2eec83514cb65bfa01edecabb1d
Dom Henderson <dom@nethermind.io>
2026-07-06 16:43:42 -0700

    WIP

--- Clap/eDSL/Specification.lean
+++ Clap/eDSL/Specification.lean
@@ -5,21 +5,24 @@ namespace Clap
 
 namespace Spec
 
-inductive Exp (p : ℕ) where
-  | v   (_ : ℕ)
-  | c   (_ : ZMod p)
-  | add (_ _ : Exp p )
-  | mul (_ _ : Exp p )
-  | sub (_ _ : Exp p )
-  deriving DecidableEq
+-- inductive Exp (p : ℕ) where
+--   | v   (_ : ℕ)
+--   | c   (_ : ZMod p)
+--   | add (_ _ : Exp p )
+--   | mul (_ _ : Exp p )
+--   | sub (_ _ : Exp p )
+--   deriving DecidableEq
 
-inductive Circuit (p : ℕ) : Type where
-  | nil
-  | eq0 (e : Exp p) (c : Circuit p)
-  | lam (cont : ℕ → Circuit p)
-  | share (e : Exp p) (cont : ℕ → Circuit p)
-  | isZero (e : Exp p) (cont : ℕ → Circuit p)
-  | num2bits (w : ℕ) (e : Exp p) (cont : List ℕ → Circuit p)
+abbrev Exp (p: ℕ) := Clap.Exp p ℕ
+
+-- inductive Circuit (p : ℕ) : Type where
+--   | nil
+--   | eq0 (e : Exp p) (c : Circuit p)
+--   | lam (cont : ℕ → Circuit p)
+--   | share (e : Exp p) (cont : ℕ → Circuit p)
+--   | isZero (e : Exp p) (cont : ℕ → Circuit p)
+--   | num2bits (w : ℕ) (e : Exp p) (cont : List ℕ → Circuit p)
+abbrev Circuit (p: ℕ) := (Clap.Circuit p ℕ)
 
 structure AllocatedCircuit (p : ℕ) where
   varStore : Std.TreeMap ℕ (ZMod p)
@@ -41,7 +44,7 @@ def isVarAllocated {p : ℕ} (numAlloc : ℕ) (e : Exp p) : Bool :=
 --   | lam {numAlloc : ℕ} {cont : ℕ → Circuit p}
 --         (rec : WFAllocatedCircuit p ⟨numAlloc.succ, cont numAlloc⟩) :
 --         WFAllocatedCircuit p ⟨numAlloc, .lam cont⟩
---   -- | 
+--   -- |
 
 -- open Circuit in
 -- def ex : Circuit 57 := .lam fun x ↦ .lam fun y ↦ .eq0 (.v 1) .nil
@@ -58,30 +61,23 @@ def eval_expr {p : ℕ} (varStore : Std.TreeMap ℕ (ZMod p)) (e : Exp p) : Opti
   | .sub l r => do (←eval_expr varStore l) - (←eval_expr varStore r)
   | .mul l r => do (←eval_expr varStore l) * (←eval_expr varStore r)
 
-def eval {p: ℕ} (circuit : AllocatedCircuit p) : denotation (ZMod p) :=
--- Defining parametric eval like this does not seem easy
--- either the cont members in constructors like share and isZero need to be switched to exp and we
--- make eval have them pass through a .c, then prove that that's equivalent to them making a new variable
--- or we need to be able to non-destructively update the varStore, or show that it was already sufficient
--- for us to keep making new variables. This could be fixing var to nat and keeping a counter of trace length,
--- it could be making var a Fintype that keeps changing, or other options
--- There may be far reaching changes required for either updating share, isZero etc to return exps, or for
--- making additional claims about var here
-  let ⟨varStore, circuit⟩ := circuit
+-- split varstore and circuit to not have to manually prove termination
+-- TODO do we even still need AllocatedCircuit?
+def eval_impl {p: ℕ} (varStore: Std.TreeMap ℕ (ZMod p)) (circuit : Circuit p) : denotation (ZMod p) :=
   match circuit with
   | .nil =>
     .u
   | .lam k =>
     .l fun x =>
     let newIdx := varStore.size
-    eval ⟨varStore.insert newIdx x, k newIdx⟩
+    eval_impl (varStore.insert newIdx x) (k newIdx)
   | .eq0 e c =>
-    if eval_expr varStore e = .some 0 then eval ⟨varStore, c⟩ else .n
+    if eval_expr varStore e = .some 0 then eval_impl varStore c else .n
   | .share e k =>
     let newIdx := varStore.size
     let val := eval_expr varStore e
     match val with
-    | .some val => eval ⟨varStore.insert newIdx val, k newIdx⟩
+    | .some val => eval_impl (varStore.insert newIdx val) (k newIdx)
     | .none => .n
   | .isZero e k =>
     let invIdx := varStore.size
@@ -90,55 +86,93 @@ def eval {p: ℕ} (circuit : AllocatedCircuit p) : denotation (ZMod p) :=
     | .some val =>
       let inv := val⁻¹
       if val = 0
-      then eval ⟨varStore.insert invIdx inv |>.insert oIdx 1, k oIdx⟩
-      else eval ⟨varStore.insert invIdx inv |>.insert oIdx 0, k oIdx⟩
+      then eval_impl (varStore.insert invIdx inv |>.insert oIdx 1) (k oIdx)
+      else eval_impl (varStore.insert invIdx inv |>.insert oIdx 0) (k oIdx)
     | .none =>
       .n
   | .num2bits w e k =>
-      if e.eval.val < 2^w then (k (num2bitsLsbPure w e.eval)).eval else .n
+      let val := eval_expr varStore e
+      match val with
+      | .some val =>
+        if val.val < 2^w
+        then
+          let oldSize := varStore.size
+          let idxs := List.range' oldSize w
+          let bits := idxs.zip (num2bitsLsbPure w val)
+          let varStore := varStore.insertMany bits
+          eval_impl varStore (k idxs)
+        else .n
+      | .none => .n
 
-def equiv {p: ℕ} {var: Type} {ResultT: Type}
-  (varStore : var → ZMod p)
-  (circuit1 circuit2 : Edsl.CircuitContM p var ResultT)
+def eval {p: ℕ} (circuit: AllocatedCircuit p) : denotation (ZMod p) :=
+  eval_impl circuit.varStore circuit.circuit
+
+
+
+def equiv {p: ℕ} {ResultT: Type}
+  (varStore : Std.TreeMap ℕ (ZMod p))
+  (circuit1 circuit2 : Edsl.CircuitContM p ℕ ResultT)
 : Prop :=
   ∀ continuation, (
-    eval varStore (circuit1 continuation) =
-    eval varStore (circuit2 continuation)
+    eval ⟨varStore, (circuit1 continuation)⟩ =
+    eval ⟨varStore, (circuit2 continuation)⟩
   )
-  
--- instance {p : ℕ} {var : Type} {ResultT : Type}
---   : Setoid (Edsl.CircuitContM p var ResultT) where
---   r c₁ c₂ := ∀ Γ, equiv Γ c₁ c₂
---   iseqv   := {
---     refl  := by aesop (add simp equiv)
---     symm  := by aesop (add simp equiv)
---     trans := by aesop (add simp equiv)
---   }
 
-def nix {p var} (x : Exp p var) : Edsl.CircuitContM p var (Exp p var) :=
+lemma equiv_refl
+  {p : ℕ}
+  {varStore : Std.TreeMap ℕ (ZMod p)}
+  {ResultT : Type}
+  {x : Edsl.CircuitContM p ℕ ResultT}
+:
+  equiv varStore x x
+:= by
+  aesop (add simp equiv)
+
+lemma equiv_symm
+  {p : ℕ}
+  {varStore : Std.TreeMap ℕ (ZMod p)}
+  {ResultT : Type}
+  {x y : Edsl.CircuitContM p ℕ ResultT}
+:
+  equiv varStore x y → equiv varStore y x
+:= by
+  aesop (add simp equiv)
+
+lemma equiv_trans
+  {p : ℕ}
+  {varStore : Std.TreeMap ℕ (ZMod p)}
+  {ResultT : Type}
+  {x y z : Edsl.CircuitContM p ℕ ResultT}
+:
+  equiv varStore x y →
+  equiv varStore y z →
+  equiv varStore x z
+:= by
+  aesop (add simp equiv)
+
+-- TODO do we want a Setoid instance here?
+-- varstore being a parameter rather than the relation being for all varstores makes it more difficult
+
+def nix {p} (x : Exp p) : Edsl.CircuitContM p ℕ (Exp p) :=
   _root_.Pure.pure x
 
 @[simp]
-lemma _root_.Clap.Edsl.CircuitContM.nothing_def {p var} (x : Exp p var) : nix x = pure x := rfl
+lemma _root_.Clap.Edsl.CircuitContM.nothing_def
+  {p} (x : Exp p)
+: nix x = pure x := rfl
 
--- TODO prove these lemmas
--- TODO finish set of spec utility lemmas
--- TODO Setoid
 open Edsl.CircuitContM in
-lemma equiv_pure {p : ℕ} {var : Type}
-  {a b : Exp p var}
-  {varStore : var → ZMod p}
-  (h_equiv : eval_expr varStore a = eval_expr varStore b)
-: 
-  equiv varStore (pure a) (pure b)
-:= by
-  sorry
+lemma equiv_pure_unit {p : ℕ}
+  {varStore : Std.TreeMap ℕ (ZMod p)}
+:
+  equiv varStore (pure ()) (pure ())
+:= equiv_refl
 
-lemma equiv_eq0 {p: ℕ} {var: Type} {ResultT: Type}
-  (varStore : var → ZMod p)
-  (a: Exp p var)
-  (rest: Edsl.CircuitContM p var ResultT)
-  (h_equiv: eval_expr varStore a = 0)
+lemma equiv_eq0 {p: ℕ} {ResultT: Type}
+  {varStore : Std.TreeMap ℕ (ZMod p)}
+  (a: Exp p)
+  (rest : Edsl.CircuitContM p ℕ ResultT)
+  (h_equiv: eval_expr varStore a = .some 0)
 :
   equiv
     varStore
@@ -148,40 +182,67 @@ lemma equiv_eq0 {p: ℕ} {var: Type} {ResultT: Type}
     )
     rest
 := by
-  sorry
+  simp [equiv]
+  intro continuation
+  simp [eval, bind, Edsl.eq0, eval_impl, h_equiv]
 
-def fails {p: ℕ} {var: Type} {ResultT: Type}
-  (varStore : var → ZMod p)
-  (circuit : Edsl.CircuitContM p var ResultT)
+-- TODO do we want Edsl.share to return an exp?
+lemma equiv_share {p: ℕ} {ResultT: Type} {val : ZMod p} {other}
+  {varStore : Std.TreeMap ℕ (ZMod p)}
+  (a: Exp p)
+  (rest: (Exp p) → Edsl.CircuitContM p ℕ ResultT)
+  (h_a : eval_expr varStore a = .some val)
+:
+  equiv
+    varStore
+    (do
+      let x ← Edsl.share a
+      rest (.v x)
+    )
+    other =
+  equiv
+    (varStore.insert (varStore.size) val)
+    (rest (.v varStore.size))
+    other
+:= by
+  simp [equiv, bind, Edsl.share, eval, eval_impl, h_a]
+  apply Iff.intro
+  . intro h continuation
+    replace h := h continuation
+    rw [h]
+    done
+
+def fails {p: ℕ} {ResultT: Type}
+  (varStore : Std.TreeMap ℕ (ZMod p))
+  (circuit : Edsl.CircuitContM p ℕ ResultT)
 : Prop :=
-  ∀ continuation,
-    eval varStore (circuit continuation) =
+    eval ⟨varStore, circuit (λ _result => .nil)⟩ =
     .n
 
-lemma fails_eq0 {p: ℕ} {var: Type} {ResultT: Type}
-  (varStore : var → ZMod p)
-  (a: Exp p var)
-  (h_equiv: eval_expr varStore a ≠ 0)
+lemma fails_eq0 {p: ℕ}
+  (varStore : Std.TreeMap ℕ (ZMod p))
+  (a: Exp p)
+  (h_equiv: eval_expr varStore a ≠ .some 0)
 :
   fails
     varStore
     (Edsl.eq0 a)
 := by
-  sorry
+  simp [fails, eval, Edsl.eq0, eval_impl, h_equiv]
 
-lemma fails_of_head_fails {p: ℕ} {var: Type} {MidT ResultT: Type}
-  (varStore : var → ZMod p)
-  (head: Edsl.CircuitContM p var MidT)
-  (tail: MidT → Edsl.CircuitContM p var ResultT)
-  (h_fails: fails varStore head)
+lemma fails_of_head_fails {p: ℕ} {ResultT: Type}
+  (varStore : Std.TreeMap ℕ (ZMod p))
+  (a: Exp p)
+  (tail: Unit → Edsl.CircuitContM p ℕ ResultT)
+  (h_fails: fails varStore (Edsl.eq0 a))
 :
   fails
     varStore
-    (bind head tail)
+    (bind (Edsl.eq0 a) tail)
 := by
-  simp [fails, bind] at ⊢ h_fails
-  intro continuation
-  rw [h_fails]
+  simp [fails, Edsl.eq0] at ⊢ h_fails
+  simp [eval, eval_impl] at h_fails
+  simp [h_fails, Edsl.eq0, eval, eval_impl, bind]
 
 -- TODO
 -- this is not true in general
@@ -255,7 +316,7 @@ lemma equiv_pure {p : ℕ} {var : Type}
   {a b : Exp p var}
   {varStore : var → ZMod p}
   (h_equiv : (eval_expr a).run' varStore = (eval_expr b).run' varStore)
-: 
+:
   equiv varStore (pure a) (pure b)
 := by
   sorry
